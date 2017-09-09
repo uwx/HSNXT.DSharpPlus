4bfcc10c03e5b28fa2a25066a0652a060429b466
uwx <chris331hansen@outlook.com>
2017-09-08 22:23:38 -0300

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -1,11 +1,12 @@
-dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.CommandsNext/DSharpPlus.CommandsNext.csproj
-dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.Interactivity/DSharpPlus.Interactivity.csproj
-dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.Rest/DSharpPlus.Rest.csproj
-dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.Test/DSharpPlus.Test.csproj
-dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.VoiceNext/DSharpPlus.VoiceNext.csproj
-dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.WebSocket.WebSocket4Net/DSharpPlus.WebSocket.WebSocket4Net.csproj
-dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus/DSharpPlus.csproj
+dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.CommandsNext/DSharpPlus.CommandsNext.csproj || exit 1
+dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.Interactivity/DSharpPlus.Interactivity.csproj || exit 1
+dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.Rest/DSharpPlus.Rest.csproj || exit 1
+dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.VoiceNext/DSharpPlus.VoiceNext.csproj || exit 1
+dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.WebSocket.WebSocket4Net/DSharpPlus.WebSocket.WebSocket4Net.csproj || exit 1
+dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus/DSharpPlus.csproj || exit 1
 
 if [ "$COREAPPTYP" == "net45" ] || [ "$COREAPPTYP" == "net46" ] || [ "$COREAPPTYP" == "net47" ]; then
-  dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.WebSocket.WebSocketSharp/DSharpPlus.WebSocket.WebSocketSharp.csproj
+  dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.WebSocket.WebSocketSharp/DSharpPlus.WebSocket.WebSocketSharp.csproj || exit 1
 fi
+
+dotnet publish $CORESC -f netcoreapp1.1 -c $COREBTYP DSharpPlus.Test/DSharpPlus.Test.csproj
