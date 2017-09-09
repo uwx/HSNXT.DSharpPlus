6cb776e94b7dfaa0c55557174038e453eb44941c
uwx <chris331hansen@outlook.com>
2017-09-08 22:24:51 -0300

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -5,8 +5,6 @@ dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.VoiceNext/DSharpPl
 dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.WebSocket.WebSocket4Net/DSharpPlus.WebSocket.WebSocket4Net.csproj || exit 1
 dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus/DSharpPlus.csproj || exit 1
 
-if [ "$COREAPPTYP" == "net45" ] || [ "$COREAPPTYP" == "net46" ] || [ "$COREAPPTYP" == "net47" ]; then
-  dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.WebSocket.WebSocketSharp/DSharpPlus.WebSocket.WebSocketSharp.csproj || exit 1
-fi
+dotnet publish $CORESC -f $COREAPPTYP -c $COREBTYP DSharpPlus.WebSocket.WebSocketSharp/DSharpPlus.WebSocket.WebSocketSharp.csproj || exit 1
 
 dotnet publish $CORESC -f netcoreapp1.1 -c $COREBTYP DSharpPlus.Test/DSharpPlus.Test.csproj
