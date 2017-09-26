677045bc9b3fb3f671bdc7a0dffe01baa75d00d5
chrishansen69 <cadrekucra@gmx.com>
2017-09-26 17:18:38 -0300

    cleany

---
+++ TestProj47/Dff/DateTimeExtensions.cs
@@ -0,0 +1,53 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: dff.Extensions.DateTimeExtensions
+// Assembly: dff.Extensions, Version=1.12.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: D6C927DF-93D7-4A34-9061-9B93EC850F98
+// Assembly location: C:\Users\Rafael\Documents\GitHub\TestProject\TestProj47\bin\Debug\dff.Extensions.dll
+
+using System;
+using System.Globalization;
+
+namespace TestProj47
+{
+    public static partial class Extensions
+    {
+        public static string GetDateTimeString(this DateTime date)
+        {
+            try
+            {
+                var provider = (IFormatProvider) new CultureInfo("de-DE", true);
+                return date.ToString("G", provider);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        public static string GetDateTimeStringIso(this DateTime date)
+        {
+            try
+            {
+                return date.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        public static TimeSpan GetTimeSpanTillNow(this DateTime date)
+        {
+            return new TimeSpan(DateTime.Now.Ticks - date.Ticks);
+        }
+
+        public static TimeSpan GetTimeSpanTillNow(this DateTime? date)
+        {
+            var dateTime = DateTime.Now;
+            var ticks1 = dateTime.Ticks;
+            dateTime = Convert.ToDateTime(date);
+            var ticks2 = dateTime.Ticks;
+            return new TimeSpan(ticks1 - ticks2);
+        }
+    }
+}
---
+++ TestProj47/Dff/ObjectExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: dff.Extensions.ObjectExtensions
+// Assembly: dff.Extensions, Version=1.12.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: D6C927DF-93D7-4A34-9061-9B93EC850F98
+// Assembly location: C:\Users\Rafael\Documents\GitHub\TestProject\TestProj47\bin\Debug\dff.Extensions.dll
+
+using System;
+using dff.Extensions.HelperClasses;
+
+namespace TestProj47
+{
+    public static partial class Extensions
+    {
+        public static string Dump(this object obj)
+        {
+            try
+            {
+                return ObjectDumper.Dump(obj);
+            }
+            catch (Exception ex)
+            {
+                return "Error while dumping: " + ex.Message;
+            }
+        }
+    }
+}
