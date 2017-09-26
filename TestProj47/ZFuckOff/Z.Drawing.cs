900dc6588463c4b133e95ebf38c17c116c9cfda5
chrishansen69 <cadrekucra@gmx.com>
2017-09-26 14:31:16 -0300

    remove unused using directives

--- TestProj47/ZFuckOff/Z.Drawing.cs
+++ TestProj47/ZFuckOff/Z.Drawing.cs
@@ -1,41 +1,8 @@
 using System;
-using System.Linq.Expressions;
-using System.Runtime.Caching;
-using System.Collections.Generic;
-using System.Linq;
-using System.Dynamic;
-using System.Collections.Specialized;
-using System.Collections;
-using System.IO;
-using System.IO.Compression;
-using System.Text;
-using System.Data;
 using System.Drawing;
-using System.Web;
-using System.Globalization;
-using System.ComponentModel;
-using System.Net;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Reflection;
-using System.Security.Cryptography;
-using System.Text.RegularExpressions;
-using System.Data.Entity.Design.PluralizationServices;
-using System.Security;
-using System.Xml.Linq;
-using System.Xml;
-using System.Collections.ObjectModel;
-using System.Data.Common;
 //using System.Data.SqlServerCe;
 using System.Drawing.Drawing2D;
-using System.Security.AccessControl;
-using System.Net.Mail;
-using System.Runtime.CompilerServices;
-using System.Web.Script.Serialization;
-using System.Runtime.Serialization.Json;
-using System.Xml.Serialization;
-using System.Web.UI;
-using System.Windows.Forms;
+
 // Description: C# Extension Methods Library to enhances the .NET Framework by adding hundreds of new methods. It drastically increases developers productivity and code readability. Support C# and VB.NET
 // Website & Documentation: https://github.com/zzzprojects/Z.ExtensionMethods
 // Forum: https://github.com/zzzprojects/Z.ExtensionMethods/issues
