8083baeeb11b301ea4f16aa79ab58d6ba3c29edb
uwx <chris331hansen@outlook.com>
2017-11-06 02:56:23 -0200

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -6,7 +6,7 @@ if [ "$COREBTYP" != "BuildAlt" ]; then
   dotnet publish $CORESC --output ./travis-bin/ -f $COREAPPTYP -c $COREBTYP DSharpPlus/DSharpPlus.csproj || exit 1
 
   if [ "$COREAPPTYP" != "netstandard1.1" ]; then
-    dotnet publish $CORESC --output ./travis-bin/ -f $COREAPPTYP -c $COREBTYP DSharpPlus.WebSocket.WebSocket4Net/DSharpPlus.WebSocket.WebSocket4Net.csproj || exit 1
+    dotnet publish $CORESC --output ./travis-bin/ -f $COREAPPTYP -c $COREBTYP DSharpPlus.WebSocket.WebSocket4NetCore/DSharpPlus.WebSocket.WebSocket4NetCore.csproj || exit 1
   fi
 else
   dotnet build $CORESC -v Minimal -c Release --version-suffix "$TRAVIS_BUILD_NUMBER"
