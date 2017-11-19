8c0711b4ebead0b11b614676f9847623f964bf75
uwx <cadrekucra@gmx.com>
2017-11-18 22:43:19 -0200

    add ReflectToArray

---
+++ ReflectToArrayTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using HSNXT;
+
+namespace ReflectToArrayTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] outs;
+            Console.WriteLine(outs = new List<string>
+            {
+                "fiddly",
+                "hoo"
+            }.ReflectToArray()); // System.String[]
+            
+            Console.WriteLine(outs.Length); // 4 (because of how List expands)
+            Console.WriteLine(outs[0]); // "fiddly"
+            Console.WriteLine(outs[1]); // "hoo"
+        }
+    }
+}
