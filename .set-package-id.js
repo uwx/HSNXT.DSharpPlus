bb9eeecfd05a600cdb5985353f806bc608de1f2e
uwx <cadrekucra@gmx.com>
2017-12-20 16:50:07 -0200

    NOW i see it

--- .set-package-id.js
+++ .set-package-id.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 const read = fs.readFileSync(process.argv[2], 'utf8');
 fs.writeFileSync(process.argv[2],
   read.includes('PackageId')
-    ? read.replace(/<PackageId>.*?</PackageId>/g, `<PackageId>${process.argv[3]}</PackageId>`)
+    ? read.replace(/<PackageId>.*?<\/PackageId>/g, `<PackageId>${process.argv[3]}</PackageId>`)
     : read.replace(/<PropertyGroup>/, `<PropertyGroup><PackageId>${process.argv[3]}</PackageId>`)
 );
