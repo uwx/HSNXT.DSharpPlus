8b79b1d9591a7b398b5439d7c339234f77ad76ef
uwx <cadrekucra@gmx.com>
2017-12-20 18:28:29 -0200

    add HSNXT.Promise

---
+++ HSNXT.Promise/Class1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HSNXT.Promise
+{
+    public class Promise<T>
+    {
+        public static Task<T> New(Action<Action<T>, Action<Exception>> executor)
+        {
+            var completionSource = new TaskCompletionSource<T>();
+            executor(result =>
+            {
+                completionSource.SetResult(result);
+            }, exception =>
+            {
+                completionSource.SetException(exception);
+            });
+            return completionSource.Task;
+        }
+        public static Task<T> New(Action<Action<T>, Action> executor)
+        {
+            var completionSource = new TaskCompletionSource<T>();
+            executor(result =>
+            {
+                completionSource.SetResult(result);
+            }, () =>
+            {
+                completionSource.SetCanceled();
+            });
+            return completionSource.Task;
+        }
+        public static Task<T> New(Func<Action<T>, Action<Exception>, Task> executor)
+        {
+            var completionSource = new TaskCompletionSource<T>();
+            executor(result =>
+            {
+                completionSource.SetResult(result);
+            }, exception =>
+            {
+                completionSource.SetException(exception);
+            });
+            return completionSource.Task;
+        }
+        public static Task<T> New(Func<Action<T>, Action, Task> executor)
+        {
+            var completionSource = new TaskCompletionSource<T>();
+            executor(result =>
+            {
+                completionSource.SetResult(result);
+            }, () =>
+            {
+                completionSource.SetCanceled();
+            });
+            return completionSource.Task;
+        }
+        
+        public static Task New(Action<Action, Action<Exception>> executor)
+        {
+            var completionSource = new TaskCompletionSource<bool>();
+            executor(() =>
+            {
+                completionSource.SetResult(true);
+            }, exception =>
+            {
+                completionSource.SetException(exception);
+            });
+            return completionSource.Task;
+        }
+        public static Task New(Action<Action, Action> executor)
+        {
+            var completionSource = new TaskCompletionSource<bool>();
+            executor(() =>
+            {
+                completionSource.SetResult(true);
+            }, () =>
+            {
+                completionSource.SetCanceled();
+            });
+            return completionSource.Task;
+        }
+        public static Task New(Func<Action, Action<Exception>, Task> executor)
+        {
+            var completionSource = new TaskCompletionSource<bool>();
+            executor(() =>
+            {
+                completionSource.SetResult(true);
+            }, exception =>
+            {
+                completionSource.SetException(exception);
+            });
+            return completionSource.Task;
+        }
+        public static Task New(Func<Action, Action, Task> executor)
+        {
+            var completionSource = new TaskCompletionSource<bool>();
+            executor(() =>
+            {
+                completionSource.SetResult(true);
+            }, () =>
+            {
+                completionSource.SetCanceled();
+            });
+            return completionSource.Task;
+        }
+    }
+}
