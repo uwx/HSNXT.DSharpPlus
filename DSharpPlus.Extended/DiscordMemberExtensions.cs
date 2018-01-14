6e850b757246a128bd834e0f7b796d50feefa7db
uwx <cadrekucra@gmx.com>
2018-01-14 16:45:44 -0200

    new extensions

--- DSharpPlus.Extended/DiscordMemberExtensions.cs
+++ DSharpPlus.Extended/DiscordMemberExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using DSharpPlus.Entities;
 
@@ -33,13 +34,48 @@ namespace DSharpPlus.Extended
             string reason = null)
         {
             if (voiceChannel.Type != ChannelType.Voice)
+            {
                 throw new ArgumentException("Given channel is not a voice channel.", nameof(voiceChannel));
-            
+            }
+
             return @this.ModifyAsync(e =>
             {
                 e.VoiceChannel = voiceChannel;
                 e.AuditLogReason = reason;
             });
         }
+
+        /// <summary>
+        /// Checks if a given member can interact with another member (kick, ban, modify permissions).
+        /// Note that this only checks the role position and not the actual permission.
+        /// </summary>
+        /// <param name="this">this object</param>
+        /// <param name="target">the member to check against</param>
+        /// <returns>true if this member can interact with the target</returns>
+        /// <exception cref="ArgumentNullException">if target is null</exception>
+        public static bool CanInteract(DiscordMember @this, DiscordMember target)
+        {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+
+            var guild = @this.Guild;
+            if (guild != target.Guild)
+            {
+                throw new ArgumentException("Provided members must both be Member objects of the same Guild!",
+                    nameof(target));
+            }
+
+            if (guild.Owner == @this) return true;
+            if (guild.Owner == target) return false;
+
+            var issuerRole = @this.Roles.FirstOrDefault();
+            var targetRole = target.Roles.FirstOrDefault();
+            return issuerRole != null && (targetRole == null || issuerRole.CanInteract(targetRole));
+        }
+
+        /// <summary>
+        /// Gets this member's permissions in the guild they're part of.
+        /// </summary>
+        public static Permissions GetPermissions(DiscordMember @this) =>
+            @this.Roles.Select(e => e.Permissions).Aggregate((a, b) => a | b);
     }
 }
---
+++ DSharpPlus.Extended/DiscordRoleExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DSharpPlus.Entities;
+
+namespace DSharpPlus.Extended
+{
+    public static class DiscordRoleExtensions
+    {
+        /// <summary>
+        /// Checks if a given role can interact with another role (kick, ban, modify permissions).
+        /// Note that this only checks the role position and not the actual permission.
+        /// </summary>
+        /// <param name="this">this object</param>
+        /// <param name="target">the role to check against</param>
+        /// <returns>true if this role can interact with the target</returns>
+        /// <exception cref="ArgumentNullException">if target is null</exception>
+        public static bool CanInteract(this DiscordRole @this, DiscordRole target)
+        {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+            
+            return target.Position < @this.Position;
+        }
+
+    }
+}
