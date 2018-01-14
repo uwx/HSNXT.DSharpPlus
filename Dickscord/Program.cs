285f88eb3d0f0e478ae2977ff5afcb2d7720b07e
uwx <cadrekucra@gmx.com>
2018-01-14 16:45:44 -0200

    new extensions

--- Dickscord/Program.cs
+++ Dickscord/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//#define ShortestPossibleName
+#define JsMode
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -16,9 +19,11 @@ namespace Dickscord
             var dict = (IReadOnlyDictionary<string, string>) typeof(DiscordEmoji).GetProperty("UnicodeEmojis",
                     BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.FlattenHierarchy)
                 .GetValue(null, null);
-
+#if JsMode
+            var ar = dict.Select(kv => $@"""{kv.Key.Replace(@"\", @"\\")}"": ""{Cv(kv.Value)}"",");
+#else
             var ar = dict.Select(kv => $@"                [""{kv.Key.Replace(@"\", @"\\")}""] = ""{Cv(kv.Value)}"",");
-            
+#endif  
             File.WriteAllLines("./emoji.txt", ar);
         }
 
@@ -28,13 +33,20 @@ namespace Dickscord
             var asAscii = new StringBuilder();
             for (var idx = 0; idx < bytes.Length; idx += 4)
             {
+                asAscii.Append(idx == 0 ? "emoji_u" : "_");
                 var codepoint = BitConverter.ToUInt32(bytes, idx);
+#if ShortestPossibleName
                 if (codepoint <= sbyte.MaxValue)
                     asAscii.Append(Convert.ToChar(codepoint));
                 else if (codepoint <= ushort.MaxValue)
                     asAscii.AppendFormat(@"\u{0:x4}", codepoint);
                 else
+#endif
+#if JsMode
+                asAscii.AppendFormat(@"{0:x4}", codepoint);
+#else
                     asAscii.AppendFormat(@"\U{0:x8}", codepoint);
+#endif
             }
             return asAscii.ToString();
         }
