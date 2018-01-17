f33ebca2c8a7795120da1d9c8e6977373732fc81
uwx <cadrekucra@gmx.com>
2018-01-16 23:25:02 -0200

    add a destructuring test

--- TestProjTest/UnitTest1.cs
+++ TestProjTest/UnitTest1.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using HSNXT;
 using Xunit;
 
 namespace TestProjTest
@@ -7,6 +10,13 @@ namespace TestProjTest
         [Fact]
         public void Test1()
         {
+            var list = new List<string>(new[] { "1", "2", "3" });
+
+            (var a, (var b, var rest)) = list;
+            
+            a.ShouldBeEquivalentTo("1");
+            b.ShouldBeEquivalentTo("2");
+            rest.GetHead().ShouldBeEquivalentTo("3");
         }
     }
 }
