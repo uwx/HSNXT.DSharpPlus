e5214aa8be341702f0f3901a7212bb1cc525a971
uwx <cadrekucra@gmx.com>
2018-01-17 00:05:20 -0200

    organize d#+ extended further

--- DSharpPlus.Extended/ExtensionMethods/ChannelExtensions.cs
+++ DSharpPlus.Extended/ExtensionMethods/ChannelExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using DSharpPlus.Entities;
 
@@ -8,108 +7,6 @@ namespace DSharpPlus.Extended
 {
     public static class ChannelExtensions
     {
-        /// <summary>
-        /// Renames the current channel.
-        /// </summary>
-        /// <param name="this">This object</param>
-        /// <param name="name">New name for the channel.</param>
-        /// <param name="reason">Reason for audit logs.</param>
-        /// <returns></returns>
-        public static Task RenameAsync(this DiscordChannel @this, string name, string reason = null)
-        {
-            if (@this.Guild == null)
-                throw new InvalidOperationException("Cannot rename non-guild channels.");
-
-            return @this.ModifyAsync(e =>
-            {
-                e.Name = name;
-                e.AuditLogReason = reason;
-            });
-        }
-
-        /// <summary>
-        /// Sets this text channel's topic.
-        /// </summary>
-        /// <param name="this">This object</param>
-        /// <param name="topic">New topic for the channel.</param>
-        /// <param name="reason">Reason for audit logs.</param>
-        /// <returns></returns>
-        public static Task SetTopicAsync(this DiscordChannel @this, string topic, string reason = null)
-        {
-            if (@this.Guild == null)
-                throw new InvalidOperationException("Cannot set topic of non-guild channels.");
-            if (@this.Type != ChannelType.Text)
-                throw new InvalidOperationException("Cannot set topic of non-text channels.");
-
-            return @this.ModifyAsync(e =>
-            {
-                e.Topic = topic;
-                e.AuditLogReason = reason;
-            });
-        }
-
-        /// <summary>
-        /// Sets this channel's parent category.
-        /// </summary>
-        /// <param name="this">This object</param>
-        /// <param name="parent">Category to put this channel in.</param>
-        /// <param name="reason">Reason for audit logs.</param>
-        /// <returns></returns>
-        public static Task SetParentAsync(this DiscordChannel @this, Optional<DiscordChannel> parent,
-            string reason = null)
-        {
-            if (@this.Guild == null)
-                throw new InvalidOperationException("Cannot set parent of non-guild channels.");
-
-            return @this.ModifyAsync(e =>
-            {
-                e.Parent = parent;
-                e.AuditLogReason = reason;
-            });
-        }
-
-        /// <summary>
-        /// Sets this voice channel's bitrate in kbps.
-        /// </summary>
-        /// <param name="this">This object</param>
-        /// <param name="bitrate">New voice bitrate for the channel.</param>
-        /// <param name="reason">Reason for audit logs.</param>
-        /// <returns></returns>
-        public static Task SetBitrateAsync(this DiscordChannel @this, int bitrate, string reason = null)
-        {
-            if (@this.Guild == null)
-                throw new InvalidOperationException("Cannot set user limit of non-guild channels.");
-            if (@this.Type != ChannelType.Voice)
-                throw new InvalidOperationException("Cannot set user limit of non-voice channels.");
-
-            return @this.ModifyAsync(e =>
-            {
-                e.Bitrate = bitrate;
-                e.AuditLogReason = reason;
-            });
-        }
-
-        /// <summary>
-        /// Sets this voice channel's connected user limit.
-        /// </summary>
-        /// <param name="this">This object</param>
-        /// <param name="userLimit">New user limit for the channel.</param>
-        /// <param name="reason">Reason for audit logs.</param>
-        /// <returns></returns>
-        public static Task SetUserLimitAsync(this DiscordChannel @this, int userLimit, string reason = null)
-        {
-            if (@this.Guild == null)
-                throw new InvalidOperationException("Cannot set user limit of non-guild channels.");
-            if (@this.Type != ChannelType.Voice)
-                throw new InvalidOperationException("Cannot set user limit of non-voice channels.");
-
-            return @this.ModifyAsync(e =>
-            {
-                e.Userlimit = userLimit;
-                e.AuditLogReason = reason;
-            });
-        }
-
         /// <summary>
         /// Deletes multiple messages without the 100 message limit
         /// </summary>
--- DSharpPlus.Extended/ExtensionMethods/MemberExtensions.cs
+++ DSharpPlus.Extended/ExtensionMethods/MemberExtensions.cs
@@ -1,50 +1,11 @@
 ﻿using System;
 using System.Linq;
-using System.Threading.Tasks;
 using DSharpPlus.Entities;
 
 namespace DSharpPlus.Extended
 {
     public static class MemberExtensions
     {
-        /// <summary>
-        /// Renames this member.
-        /// </summary>
-        /// <param name="this">This object</param>
-        /// <param name="nickname">Nickname to set for this member.</param>
-        /// <param name="reason">Reason for audit logs.</param>
-        /// <returns></returns>
-        public static Task RenameAsync(this DiscordMember @this, string nickname, string reason = null)
-        {
-            return @this.ModifyAsync(e =>
-            {
-                e.Nickname = nickname;
-                e.AuditLogReason = reason;
-            });
-        }
-
-        /// <summary>
-        /// Move this member to a voice channel.
-        /// </summary>
-        /// <param name="this">This object</param>
-        /// <param name="voiceChannel">Voice channel to put the member into.</param>
-        /// <param name="reason">Reason for audit logs.</param>
-        /// <returns></returns>
-        public static Task MoveToAsync(this DiscordMember @this, DiscordChannel voiceChannel,
-            string reason = null)
-        {
-            if (voiceChannel.Type != ChannelType.Voice)
-            {
-                throw new ArgumentException("Given channel is not a voice channel.", nameof(voiceChannel));
-            }
-
-            return @this.ModifyAsync(e =>
-            {
-                e.VoiceChannel = voiceChannel;
-                e.AuditLogReason = reason;
-            });
-        }
-
         /// <summary>
         /// Checks if a given member can interact with another member (kick, ban, modify permissions).
         /// Note that this only checks the role position and not the actual permission.
