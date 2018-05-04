bb3ec355bbdcd5692441c947ca8c2fdd9f733cee
uwx <cadrekucra@gmx.com>
2018-05-04 03:36:58 -0300

    move everything

--- RiderPlugin/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RiderPlugin
-{
-    public class Class1
-    {
-    }
-}
--- RiderTestAssembly/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RiderTestAssembly
-{
-    public class Class1
-    {
-    }
-}
