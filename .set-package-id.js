9b8f43e2ddaec88e531d23fde7e2533283706b92
uwx <cadrekucra@gmx.com>
2018-07-04 01:41:00 -0300

    disable packageid setting

--- .set-package-id.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const read = fs.readFileSync(process.argv[2], 'utf8');
-fs.writeFileSync(process.argv[2],
-  read.includes('PackageId')
-    ? read.replace(/<PackageId>.*?<\/PackageId>/g, `<PackageId>${process.argv[3]}</PackageId>`)
-    : read.replace(/<PropertyGroup>/, `<PropertyGroup><PackageId>${process.argv[3]}</PackageId>`)
-);
