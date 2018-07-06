a77f42505083b4e382b55d242f3a9e492f05a3e6
uwx <cadrekucra@gmx.com>
2018-07-06 03:24:41 -0300

    Create RequireRoleOrHigherAttribute.cs

---
+++ HSNXT.DSharpPlus.Extended/CNext/RequireRoleOrHigherAttribute.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSharpPlus.Extended.CNext
+{
+    /// <summary>
+    /// Defines that usage of this command is restricted to members with specified role, or higher.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class RequireRoleOrHigherAttribute : CheckBaseAttribute
+    {
+        /// <summary>
+        /// Gets the name of the role required to execute this command.
+        /// </summary>
+        public string RoleName { get; }
+
+        /// <summary>
+        /// Defines that usage of this command is restricted to members with specified role, or higher.
+        /// </summary>
+        /// <param name="roleName">Name of the role to be verified by this check.</param>
+        public RequireRoleOrHigherAttribute(string roleName) => RoleName = roleName;
+
+        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+        {
+            if (ctx.Guild == null || ctx.Member == null)
+                return Task.FromResult(false);
+
+            var targetPosition = ctx.Guild.GetRole(RoleName).Position;
+            // avoid multiple enumeration
+            var ourPosition = ctx.Member.Roles.OrderByDescending(e => e.Position).DefaultIfEmpty(int.MaxValue).First();
+            
+            if (ourPosition == int.MaxValue) // roles can never go this high
+              return Task.FromResult(false);
+            
+            return Task.FromResult(ourPosition >= targetPosition);
+        }
+    }
+}
