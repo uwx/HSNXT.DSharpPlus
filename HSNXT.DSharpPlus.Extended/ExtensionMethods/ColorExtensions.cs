c56ce2a363fbf06695004bb6e43cbd3dffbe0d24
uwx <cadrekucra@gmx.com>
2018-08-09 00:08:52 -0300

    create base ColorExtensions class

---
+++ HSNXT.DSharpPlus.Extended/ExtensionMethods/ColorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DSharpPlus.Entities;
+
+namespace HSNXT.DSharpPlus.Extended.ExtensionMethods
+{
+    public static class ColorExtensions
+    {
+        
+    }
+}
