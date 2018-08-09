d0146fcda120964233fc65aff4a7b42a498c6a98
uwx <cadrekucra@gmx.com>
2018-08-08 23:41:00 -0300

    deprecate DeleteManyMessagesAsync

--- HSNXT.DSharpPlus.Extended/ExtensionMethods/ChannelExtensions.cs
+++ HSNXT.DSharpPlus.Extended/ExtensionMethods/ChannelExtensions.cs
@@ -7,41 +7,10 @@ namespace HSNXT.DSharpPlus.Extended.ExtensionMethods
 {
     public static class ChannelExtensions
     {
-        /// <summary>
-        /// Deletes multiple messages without the 100 message limit
-        /// </summary>
-        /// <param name="chan">This object</param>
-        /// <param name="messages">the messages to delete</param>
-        /// <param name="reason">Reason for audit logs.</param>
-        /// <returns></returns>
-        public static async Task DeleteManyMessagesAsync(this DiscordChannel chan, IEnumerable<DiscordMessage> messages,
+        [Obsolete("Update to 4.0.0-beta-484 and use DeleteMessagesAsync instead")]
+        public static Task DeleteManyMessagesAsync(this DiscordChannel chan, IEnumerable<DiscordMessage> messages,
             string reason = null)
-        {
-            // don't enumerate more than once
-            var msgs = messages as List<DiscordMessage> ?? new List<DiscordMessage>(messages);
-
-            // ReSharper disable once ConvertIfStatementToSwitchStatement
-            if (msgs.Count == 0)
-            {
-                return;
-            }
-
-            if (msgs.Count == 1)
-            {
-                await chan.DeleteMessageAsync(msgs[0], reason);
-            }
-            else if (msgs.Count <= 100)
-            {
-                await chan.DeleteMessagesAsync(msgs, reason);
-            }
-            else
-            {
-                foreach (var msgsSub in SplitList(msgs, 100))
-                {
-                    await chan.DeleteMessagesAsync(msgs, reason);
-                }
-            }
-        }
+            => chan.DeleteMessagesAsync(messages, reason);
 
         [Obsolete("Update to 4.0.0-beta-481 and use GetMessagesBeforeAsync instead")]
         public static Task<IReadOnlyList<DiscordMessage>> GetManyMessagesBeforeAsync(this DiscordChannel chan,
@@ -56,13 +25,5 @@ namespace HSNXT.DSharpPlus.Extended.ExtensionMethods
         [Obsolete("Update to 4.0.0-beta-481 and use GetMessagesAsync instead")]
         public static Task<IReadOnlyList<DiscordMessage>> GetManyMessagesAsync(this DiscordChannel chan, int amount)
             => chan.GetMessagesAsync(amount);
-        
-        private static IEnumerable<List<T>> SplitList<T>(List<T> locations, int nSize)
-        {
-            for (var i = 0; i < locations.Count; i += nSize)
-            {
-                yield return locations.GetRange(i, Math.Min(nSize, locations.Count - i));
-            }
-        }
     }
 }
