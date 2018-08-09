5a68d08e84222d37c3f440c7354f1047445af79c
uwx <cadrekucra@gmx.com>
2018-08-09 00:08:45 -0300

    clean up EmptyList some

--- HSNXT.DSharpPlus.Extended/EmptyList.cs
+++ HSNXT.DSharpPlus.Extended/EmptyList.cs
@@ -5,15 +5,47 @@ using DSharpPlus.Entities;
 
 namespace HSNXT.DSharpPlus.Extended
 {
-    internal class EmptyList<T> : IReadOnlyList<DiscordMessage>
+    internal readonly struct EmptyList<T> : IReadOnlyList<T>, IList<T>
     {
-        public IEnumerator<DiscordMessage> GetEnumerator()
+        private readonly struct EmptyEnumerator : IEnumerator<T>
         {
-            yield break;
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext() => false;
+
+            public void Reset()
+            {
+            }
+
+            public T Current => default;
+
+            object IEnumerator.Current => default;
         }
 
+        public IEnumerator<T> GetEnumerator() => new EmptyEnumerator();
+
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
+        public void Add(T item) => throw new InvalidOperationException();
+        public void Clear() => throw new InvalidOperationException();
+        public bool Contains(T item) => false;
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+        }
+        public bool Remove(T item) => throw new InvalidOperationException();
+        public int IndexOf(T item) => -1;
+        public void Insert(int index, T item) => throw new InvalidOperationException();
+        public void RemoveAt(int index) => throw new InvalidOperationException();
+
         public int Count => 0;
-        public DiscordMessage this[int index] => throw new IndexOutOfRangeException();
+        public bool IsReadOnly => true;
+        
+        public T this[int index]
+        {
+            get => throw new IndexOutOfRangeException();
+            set => throw new InvalidOperationException();
+        }
     }
 }
