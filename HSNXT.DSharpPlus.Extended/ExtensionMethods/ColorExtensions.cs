ec4b32a3759066e478871e006fc2f1acdf0be1a7
uwx <cadrekucra@gmx.com>
2018-08-10 23:28:12 -0300

    add ToHexString method for discord colors

--- HSNXT.DSharpPlus.Extended/ExtensionMethods/ColorExtensions.cs
+++ HSNXT.DSharpPlus.Extended/ExtensionMethods/ColorExtensions.cs
@@ -7,6 +7,11 @@ namespace HSNXT.DSharpPlus.Extended.ExtensionMethods
 {
     public static class ColorExtensions
     {
-        
+        /// <summary>
+        /// Converts a <see cref="DiscordColor"/> object into a hexadecimal string without a leading hash symbol.
+        /// </summary>
+        /// <param name="c">The color</param>
+        /// <returns>The hexadecimal color string</returns>
+        public static string ToHexString(this DiscordColor c) => $"{c.R:X2}{c.G:X2}{c.B:X2}";
     }
 }
