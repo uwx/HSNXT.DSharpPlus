209e3e3cfafd58aaadbe661916c6df87f4d6d2e0
Appveyor Automated Build <appveyor@ci.org>
2018-08-14 16:16:43 -0700

    Generated docs [skip ci]

---
+++ sharpdox/Html/default/data/Navigation.js
@@ -0,0 +1,10 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.navigationData = [
+			
+
+
+
+	];
---
+++ sharpdox/Html/default/data/Project.js
@@ -0,0 +1,16 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.projectData = {
+  name: 'HSNXT.DSharpPlus',
+   homepage: 'https://github.com/uwx/HSNXT.DSharpPlus',      author: 'uwx',    authorHomepage: 'https://github.com/uwx',     showCode: false,
+    hasLogo: false,
+  targetFxs: [
+			"",
+			"netstandard2.0",
+			"netcoreapp2.0",
+			"net47",
+			"netcoreapp2.1",
+	  ]
+}
