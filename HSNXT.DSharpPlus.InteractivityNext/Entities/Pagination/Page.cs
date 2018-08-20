2a861bc36bc4b65cc970f80ded09d4aff899b0cb
uwx <cadrekucra@gmx.com>
2018-08-20 06:51:09 -0300

    make page a struct; add docs

--- HSNXT.DSharpPlus.InteractivityNext/Entities/Pagination/Page.cs
+++ HSNXT.DSharpPlus.InteractivityNext/Entities/Pagination/Page.cs
@@ -2,9 +2,43 @@
 
 namespace DSharpPlus.Interactivity
 {
-    public class Page
+    /// <summary>
+    /// Represents an individual page for <see cref="InteractivityExtension.SendPaginatedMessage"/>.
+    /// </summary>
+    public readonly struct Page
     {
-        public string Content { get; set; }
-        public DiscordEmbed Embed { get; set; }
+        public string Content { get; }
+        public DiscordEmbed Embed { get; }
+
+        /// <summary>
+        /// Creates a page containing a text message.
+        /// </summary>
+        /// <param name="content">The page contents.</param>
+        public Page(string content)
+        {
+            Content = content;
+            Embed = null;
+        }
+
+        /// <summary>
+        /// Creates a page containing an embed.
+        /// </summary>
+        /// <param name="embed">The page contents.</param>
+        public Page(DiscordEmbed embed)
+        {
+            Content = null;
+            Embed = embed;
+        }
+
+        /// <summary>
+        /// Creates a page containing both an embed and an attached message.
+        /// </summary>
+        /// <param name="content">The page text message.</param>
+        /// <param name="embed">The page embed.</param>
+        public Page(string content, DiscordEmbed embed)
+        {
+            Content = content;
+            Embed = embed;
+        }
     }
 }
