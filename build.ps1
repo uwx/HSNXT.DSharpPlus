a448debec2e1814241b716b677031d28dddc8c3e
uwx <chris331hansen@outlook.com>
2018-08-30 15:41:41 -0300

    fix build ps1 for new suffix changes

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 # Version number
 $VERSION = [int]::Parse($Env:APPVEYOR_BUILD_NUMBER).ToString("00000")
+$BUILD_SUFFIX = "nightly"
 # Environment variables
 $Env:ARTIFACT_DIR = ".\artifacts"
 # Prepare the environment
@@ -12,9 +13,9 @@ Write-Host "Artifacts will be placed in: $dir"
 # Restore NuGet packages
 dotnet restore -v Minimal
 # Build
-dotnet build DSharpPlus.sln -v Minimal -c Release --version-suffix "$VERSION"
+dotnet build DSharpPlus.sln -v Minimal -c Release --version-suffix "$BUILD_SUFFIX" -p:BuildNumber="$VERSION"
 # Package
-dotnet pack DSharpPlus.sln -v Minimal -c Release -o "$dir" --no-build --version-suffix "$VERSION"
+dotnet pack DSharpPlus.sln -v Minimal -c Release -o "$dir" --no-build --version-suffix "$BUILD_SUFFIX" -p:BuildNumber="$VERSION"
 # Remove Test packages
 # Get-ChildItem $env:ARTIFACT_DIR | ? { $_.Name.ToLower().StartsWith("DSharpPlus.Test") } | % { $_.Delete() }
 # Rebuild documentation
