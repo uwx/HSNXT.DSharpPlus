eeb6b48e3bf4f4580db359ed002d42e80a30602b
uwx <cadrekucra@gmx.com>
2019-03-23 05:01:28 -0300

    Implement client_status, activities, other docs fixes, organize serializers (fix #381)

--- DSharpPlus/Entities/Optional.cs
+++ DSharpPlus/Entities/Optional.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 using DSharpPlus.Net.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace DSharpPlus.Entities
 {
@@ -34,6 +39,7 @@ namespace DSharpPlus.Entities
     /// Represents a wrapper which may or may not have a value.
     /// </summary>
     /// <typeparam name="T">Type of the value.</typeparam>
+    [JsonConverter(typeof(OptionalJsonConverter))]
     public readonly struct Optional<T> : IEquatable<Optional<T>>, IEquatable<T>
     {
         /// <summary>
@@ -147,6 +153,97 @@ namespace DSharpPlus.Entities
         public Optional<TTarget> IfPresent<TTarget>(Func<T, TTarget> mapper)
         {
             return HasValue ? new Optional<TTarget>(mapper(Value)) : default;
+        }
+    }
+    
+    /// <seealso cref="DiscordJson.Serializer"/>
+    internal sealed class OptionalJsonContractResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var property = base.CreateProperty(member, memberSerialization);
+
+            var type = property.PropertyType;
+            
+            if (!type.GetTypeInfo().IsGenericType)
+                return property;
+
+            if (type.GetGenericTypeDefinition() != typeof(Optional<>))
+                return property;
+            
+            // we cache the PropertyInfo object here (it's captured in closure). we don't have direct 
+            // access to the property value so we have to reflect into it from the parent instance
+            // we use UnderlyingName instead of PropertyName in case the C# name is different from the Json name.
+            var optionalProp = property.DeclaringType.GetTypeInfo().GetDeclaredProperty(property.UnderlyingName);
+            
+            // DIRTY HACK to support both serializing both fields and properties, if you know a better way to do this
+            // write me at cadrekucra@gmx.com!!!!
+            var propPresent = optionalProp != null;
+            var optionalField = !propPresent
+                ? property.DeclaringType.GetTypeInfo().GetDeclaredField(property.UnderlyingName)
+                : null;
+            
+            property.ShouldSerialize = instance => // instance here is the declaring (parent) type
+            {
+                // this is the Optional<T> object
+                var optionalValue = propPresent ? optionalProp.GetValue(instance) : optionalField.GetValue(instance);
+                // get the HasValue property of the Optional<T> object and cast it to a bool, and only serialize it if
+                // it's present
+                return (bool)optionalValue.GetType().GetTypeInfo().GetDeclaredProperty("HasValue").GetValue(optionalValue);
+            };
+    
+            return property;
+        }
+    }
+
+    internal sealed class OptionalJsonConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var typeInfo = value.GetType().GetTypeInfo();
+            // we don't check for HasValue here since it's checked above
+            var val = typeInfo.GetDeclaredProperty("Value").GetValue(value);
+            // JToken.FromObject will throw if `null` so we manually write a null value.
+            if (val == null)
+            {
+                // you can read serializer.NullValueHandling here, but unfortunately you can **not** skip serialization
+                // here, or else you will get a nasty JsonWriterException, so we just ignore its value and manually
+                // write the null.
+                writer.WriteToken(JsonToken.Null);
+            }
+            else
+            {
+                // convert the value to a JSON object and write it to the property value.
+                var t = JToken.FromObject(val);
+                t.WriteTo(writer);
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
+        {
+            var genericType = objectType.GenericTypeArguments[0];
+
+            var constructor = objectType.GetTypeInfo().DeclaredConstructors
+                .Single(e => e.GetParameters()[0].ParameterType == genericType);
+            
+            try
+            {
+                return constructor.Invoke(new[] { Convert.ChangeType(reader.Value, genericType)});
+            }
+            catch
+            {
+                return existingValue;
+            }
+        }
+
+        public override bool CanRead => true;
+
+        public override bool CanConvert(Type objectType)
+        {
+            if (!objectType.GetTypeInfo().IsGenericType) return false;
+
+            return objectType.GetGenericTypeDefinition() == typeof(Optional<>);
         }
     }
 }
