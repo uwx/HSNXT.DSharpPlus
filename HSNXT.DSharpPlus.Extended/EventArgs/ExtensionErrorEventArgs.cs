9d8579a1ea2e1bb25d8b5bf70d284dc0106f551e
uwx <cadrekucra@gmx.com>
2019-05-17 01:46:59 -0300

    document ExtensionErrorEventArgs

--- HSNXT.DSharpPlus.Extended/EventArgs/ExtensionErrorEventArgs.cs
+++ HSNXT.DSharpPlus.Extended/EventArgs/ExtensionErrorEventArgs.cs
@@ -1,37 +1,28 @@
 ﻿using System;
 using DSharpPlus;
 using DSharpPlus.EventArgs;
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 
 namespace HSNXT.DSharpPlus.Extended
 {
     /// <summary>
-    /// Represents arguments for <see cref="E:DSharpPlus.DiscordClient.ExtensionErrored" /> event.
+    /// Represents arguments for the <see cref="DspExtended.ExtensionErrored"/> event.
     /// </summary>
     public class ExtensionErrorEventArgs : DiscordEventArgs
     {
-        /// <summary>
-        /// Gets the exception thrown by the client.
-        /// </summary>
+        /// <summary>Gets the exception thrown by the extension.</summary>
         public Exception Exception { get; internal set; }
 
-        /// <summary>
-        /// Gets the message of the exception thrown by the client, if available.
-        /// </summary>
+        /// <summary>Gets the message of the exception thrown by the extension, if available.</summary>
         public string Message => Exception.Message;
 
-        /// <summary>
-        /// Gets the name of the event that threw the exception.
-        /// </summary>
+        /// <summary>Gets the name of the event that threw the exception.</summary>
         public string EventName { get; internal set; }
 
-        /// <summary>
-        /// Gets the DspExtended instance which the event belongs to.
-        /// </summary>
-        public DspExtended Extension { get; set; }
+        /// <summary>Gets the DspExtended instance which the event belongs to.</summary>
+        public DspExtended Extension { get; }
         
-        /// <summary>
-        /// Gets the timestamp of the exception being caught.
-        /// </summary>
+        /// <summary>Gets the timestamp of the exception being caught.</summary>
         public DateTimeOffset Timestamp { get; } = DateTimeOffset.Now;
 
         internal ExtensionErrorEventArgs(DspExtended dspExtended) : base(dspExtended.Client)
