08a7a8bb31e484ab5d957d43528a33510132f7fe
uwx <cadrekucra@gmx.com>
2019-05-17 01:37:36 -0300

    fix doc generation and remove D#+ stuff from TOC

--- docfx_project/dsplus_template/styles/main.js
+++ docfx_project/dsplus_template/styles/main.js
@@ -1,1 +1,9 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+window.addEventListener('DOMContentLoaded', () => {
+  [...document.querySelectorAll('#toc .level1 > li')]
+    .filter(e => e.children[1].title.startsWith('DSharpPlus'))
+    .forEach(e => {
+      e.parentElement.removeChild(e);
+    })
+});
